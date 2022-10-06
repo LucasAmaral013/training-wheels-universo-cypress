db0b6b2b06ddbcb38073609a988039a0ccf14c0a
Lucas Amaral <lucasamaral013@gmail.com>
2022-10-06 09:59:17 -0300

    metodo que faz cada item do looping virar um teste

--- cypress/e2e/checkbox.spec.cy.js
+++ cypress/e2e/checkbox.spec.cy.js
@@ -1,11 +1,11 @@
-
+import 'cypress-each'
 
 it('deve marcar os top 5 filmes da marvel', function () {
     cy.visit('/checkboxes')
 
     //Eu passei um valor errado de proposito, para ver se ia continuar percorrendo o forEach e me mostrar o item com erro no final
     const movies = [
-        'avengers9',
+        'avengers',
         'cap2',
         'guardians',
         'antman',
@@ -20,3 +20,19 @@ it('deve marcar os top 5 filmes da marvel', function () {
         .should('be.checked')
     })
 })
+
+    const movies= [
+        'avengers9',
+        'cap2',
+        'guardians',
+        'antman',
+        'spider'
+    ]
+
+    it.each(movies)('deve marcar os top 5 filmes da marvel, cada filme é um teste', (m) => {
+        cy.visit('/checkboxes')
+        cy.get(`input[name=${m}]`)
+        .click()
+        .should('be.checked')
+
+    } )
